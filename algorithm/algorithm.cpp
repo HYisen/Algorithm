f9b62d27dae953715da1ad7c46a5d0b69c784985
Alex <chenhaixin@wondersgroup.com>
2017-07-18 13:03:30 +0800

    complete a dirty but efficient mergesort::merge

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -1,9 +1,29 @@
 #include "benchmark.h"
 
+#include <iostream>
+
+void test()
+{
+	using std::cout;
+	using std::endl;
+
+	int a = 4;
+	int b = 8;
+	int &ref = a;
+	cout << a << endl;
+	cout << ref << endl;
+	ref = 10;
+	cout << ref << endl;
+	cout << a << endl;
+}
+
 int main()
 {
 	auto data = generate(10, 4);
 	print(data);
+
+	test();
+
     return 0;
 }
 
--- algorithm/mergesort.h
+++ algorithm/mergesort.h
@@ -1,10 +1,57 @@
 #pragma once
 
-#include <vector>
+#include <array>
 
 namespace mergesort
 {
-	std::vector<int> sort(std::vector<int> orig);
+	using std::array;
 
-	std::vector<int> merge(std::vector<int> lhs, std::vector<int> rhs);
+	template<typename T, size_t size>
+	array<T, size> sort(const array<T, size> &orig);
+
+	//It's quite tricky to calculate size during the compiling, isn't it?
+	template<typename T, size_t ls, size_t rs>
+	array<T, ls + rs> merge(const array<T, ls> &lhs, const array<T, rs> &rhs);
+
+	template<typename T, size_t ls, size_t rs>
+	array<T, ls + rs> merge(const array<T, ls>& lhs, const array<T, rs>& rhs)
+	{
+		array<T, ls + rs> rtn{};
+		size_t li{ 0 }, ri{ 0 }, it{ 0 };
+		while (true)
+		{
+			if (lhs[li] >= rhs[ri])
+			{
+				rtn[it] = lhs[li];
+				li++;
+				if (li == lhs.size())
+				{
+					while (ri != rhs.size())
+					{
+						rtn[it] = rhs[ri];
+						it++;
+						ri++;
+					}
+					break;
+				}
+			}
+			else
+			{
+				rtn[it] = rhs[ri];
+				ri++;
+				if (ri == rhs.size())
+				{
+					while (li != lhs.size())
+					{
+						rtn[it] = lhs[li];
+						it++;
+						li++;
+					}
+					break;
+				}
+			}
+			it++;
+		}
+		return rtn;
+	}
 }
